c87941f894a75e7299a05c62166e0c6730a8417e
Moises-Salas-R <MOISESSAL4S727@GMAIL.COM>
2025-04-24 22:43:55 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jitcall.app',
+  appName: 'jitcall',
+  webDir: 'www'
+};
+
+export default config;
